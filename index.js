f841c31c3028aa2baaa08b0c1404ff54ac0ba68e
Vivek <viveksanthosh@holidayiq.com>
2020-11-06 15:29:10 +0530

    request variables

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const server = express()
 const { delay } = require('bluebird')
-const createNamespace = require('cls-hooked').createNamespace;
-const session = createNamespace('test_session');
 
-const middleware = (req, res, next) => {
-    session.run(() => {
-        set('id', req.query.id)
-        set('delayTime', req.query.delayTime)
-        next()
-    });
-    res.on('finish', () => {
-        console.log(get('status'));
+const timerMiddleware = (req, res, next) => {
+    const time = (new Date()).getTime()
+    let id = req.query.id
+    let enterTime = time
+
+    res.locals.id = id
+    res.locals.enterTime = enterTime
+
+    res.on("finish", () => {
+        const time = (new Date()).getTime()
+        const { enterTime, id } = res.locals
+        console.log(`time for ${id}->`, time - enterTime);
     })
+    next()
 }
 
-server.get('/execute', middleware, async (req, res) => {
+server.get('/execute', timerMiddleware, async (req, res) => {
+    const { delayTime, id } = req.query;
+    console.log('hit id -> ', res.locals.id );
+    await doSomething(delayTime, id)
     res.status(200).send()
-    await doSomething()
-    console.log(process.pid);
 })
 
 server.listen(4000, () => {
@@ -26,31 +30,7 @@ server.listen(4000, () => {
     console.log('started');
 })
 
-async function doSomething() {
-    const id = session.get('id');
-    const delayTime = session.get('delayTime');
-    console.log(id)
-    if (id === '1')
-        set('status', true)
-    else
-        set('status', false)
+async function doSomething(delayTime, id) {
     await delay(delayTime)
     console.log('done ', id);
-}
-
-
-function get(key) {
-    if (session && session.active) {
-        return session.get(key);
-    } else {
-        console.log('session get miss')
-    }
-}
-
-function set(key, value) {
-    if (session && session.active) {
-        return session.set(key, value);
-    } else {
-        console.log('session set miss')
-    }
 }
